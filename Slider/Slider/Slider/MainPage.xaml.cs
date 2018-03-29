cac1399f0053645dec05c1df99a52bf6388282ab
Muhammed Hedyia <hedyia@hotmail.com>
2018-03-29 20:32:00 +0200

    Sample Xamarin.forms Projects
    Co-Authored-By: hedyia <hedyia@users.noreply.github.com>

---
+++ Slider/Slider/Slider/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Slider
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            slider.Value = 0.5f;
+
+		}
+        private void ValueChangedHandler (object sender, ValueChangedEventArgs e)
+        {
+            lblValue.Text = String.Format("Value is {0:f2}", e.NewValue);
+        }
+
+    }
+}
